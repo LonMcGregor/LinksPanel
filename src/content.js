1dd679e3882bd1e01b8924fe4252debdfd346014
Léon McGregor <3817332+LonMcGregor@users.noreply.github.com>
2018-06-19 19:32:54 +0100

    Basic support

--- src/content.js
+++ src/content.js
@@ -2,3 +2,32 @@
 // get computed styles
 // send to background
 // observe for additions (and removals?)
+
+(function scrapeLinks(){
+"use strict";
+
+function processAnchor(a){
+    const computedStyle = window.getComputedStyle(a);
+    const properties = ["color", "font-family", "text-decoration", "font-weight"];
+    let style = "";
+    properties.forEach(prop => {
+        style += `${prop}: ${computedStyle.getPropertyValue(prop)};`;
+    });
+    return {
+        title: a.title,
+        text: a.innerText.trim() === "" ? a.href : a.innerText.trim(),
+        href: a.href,
+        style: style
+    };
+}
+
+function getAllAnchors(){
+    const listOfAnchors = [];
+    document.querySelectorAll("a").forEach(a => {
+        listOfAnchors.push(processAnchor(a));
+    });
+    return listOfAnchors;
+}
+
+return getAllAnchors();
+})();
--- src/panel.js
+++ src/panel.js
@@ -1,3 +1,33 @@
 // get currently active tab in currently active window
 // query the background script for the links for this tab
 // listen for changes to this
+"use strict";
+
+function makeLink(link){
+    const a = document.createElement("a");
+    a.style = link.style;
+    a.title = link.title;
+    a.href = link.href;
+    a.innerText = link.text;
+    return a;
+}
+
+function populatePanel(links){
+    const section = document.querySelector("section");
+    links.forEach(link => {
+        section.appendChild(makeLink(link));
+    });
+}
+
+// remark: may be able to get away with activeTab only if doing everything in background script browseraction on click and then querying that when panel opens
+function runOnActivePage(){
+    chrome.tabs.executeScript({
+        file: "content.js"
+    }, results => {
+        populatePanel(results[0]);
+    });
+}
+
+document.querySelector("#refresh").addEventListener("click", runOnActivePage);
+document.querySelector("#refresh").innerText = chrome.i18n.getMessage("refresh");
+document.head.title = chrome.i18n.getMessage("name");
