26c78c47f89ddd07fa25bfeb20da80b565c5f101
Léon McGregor <3817332+LonMcGregor@users.noreply.github.com>
2018-06-20 15:16:33 +0100

    Initial Working Version

--- src/content.js
+++ src/content.js
@@ -1,32 +1,88 @@
-// get all <A> tags
-// get computed styles
-// send to background
-// observe for additions (and removals?)
 (function contentScript(){
 "use strict";
 
+/* Aliases for sanity */
+const $ = document.querySelector.bind(document);
+const $a = document.querySelectorAll.bind(document);
+const $$ = document.createElement.bind(document);
+
+/* TODO
+const ANCHOR_OBSERVER = new MutationObserver(records => {
+    records.forEach(record => {
+        let added = [], removed = [];
+        record.addedNodes.forEach(node => {
+            if(node.tagName.toLowerCase() === "a"){
+                added.push(processAnchor(node));
+            }
+        });
+        record.removedNodes.forEach(node => {
+            if(node.tagName.toLowerCase() === "a"){
+                added.push(processAnchor(node));
+            }
+        });
+        console.log(added, removed);
+    });
+});
+*/
+
+/**
+ * Get relevant details out of an <a> node
+ * @param a node
+ * @returns Link detail strings {title, text, href, style}
+ */
 function processAnchor(a){
     const computedStyle = window.getComputedStyle(a);
-    const properties = ["font-family", "font-weight"]; //could have option to choose additional css properties to grab
+    const properties = ["font-family", "font-weight", "font-style"]; /* TODO could have option to choose additional css properties to grab */
     let style = "";
     properties.forEach(prop => {
         style += `${prop}: ${computedStyle.getPropertyValue(prop)};`;
     });
     return {
         title: a.title,
-        text: a.innerText.trim() === "" ? a.href : a.innerText.trim(),
+        text: a.innerText.split("\n")[0].trim() === "" ? a.href : a.innerText.split("\n")[0].trim(),
         href: a.href,
         style: style
     };
 }
 
+/**
+ * Get list of details
+ * @returns [link details]
+ */
 function getAllAnchors(){
     const listOfAnchors = [];
-    document.querySelectorAll("a").forEach(a => { //prevent duplicates
+    $a("a").forEach(a => {
         listOfAnchors.push(processAnchor(a));
     });
     return listOfAnchors;
 }
 
-return getAllAnchors();
+/**
+ * Send a message with specified links to background
+ * @param [link details]
+ */
+function sendLinksToBackground(links){
+    chrome.runtime.sendMessage({
+        updateBackground: true,
+        links: links
+    });
+}
+
+/* TODO removed and added links*/
+function sendLinksRemovedToBackground(links){
+    chrome.runtime.sendMessage();
+}
+function sendLinksAddedToBackground(links){
+    chrome.runtime.sendMessage();
+}
+
+/**
+ * Run the content script (get initial link detail array) and start the observer
+ */
+function init(){
+    sendLinksToBackground(getAllAnchors());
+    /*ANCHOR_OBSERVER.observe(document.body, {childList: true, subtree: true});*/
+}
+
+init();
 })();
--- src/panel.js
+++ src/panel.js
@@ -1,15 +1,22 @@
-// get currently active tab in currently active window
-// query the background script for the links for this tab //want to only query current tab by window
-// listen for changes to this
 "use strict";
+
+/* Aliases for sanity */
+const $ = document.querySelector.bind(document);
+const $a = document.querySelectorAll.bind(document);
+const $$ = document.createElement.bind(document);
 
 let CURRENT_LINKS;
 
+/**
+ * Create a <span> for a specific link
+ * @param link details
+ * @returns span DOM node
+ */
 function makeLink(link){
-    const span = document.createElement("span");
+    const span = $$("span");
     span.style = link.style;
     const tipTitle = link.title ? link.title : link.text;
-    span.title = `Title: ${tipTitle}
+    span.title = (tipTitle===link.href) ? link.href : `Title: ${tipTitle}
 Link: ${link.href}`;
     span.innerText = link.text;
     span.addEventListener("click", e => {
@@ -18,30 +25,34 @@ Link: ${link.href}`;
     return span;
 }
 
-function populatePanel(links){
-    const section = document.querySelector("section");
-    document.querySelectorAll("section span").forEach(span => {
+/**
+ * Remove everything from the panel section
+ */
+function emptyLinks(){
+    const section = $("section");
+    $a("section *").forEach(span => {
         section.removeChild(span);
     });
+}
+
+/**
+ * Given the array of link details,populate the panel
+ * @param [links details] to put into the panel
+ */
+function populatePanel(links){
+    emptyLinks();
+    const section = $("section");
     links.forEach(link => {
         section.appendChild(makeLink(link));
     });
 }
 
-// opera links has a search, downloads button, tooltip shows title and address, lock/unlock
-
-// remark: may be able to get away with activeTab only if doing everything in background script browseraction on click and then querying that when panel opens
-function runOnActivePage(){
-    chrome.tabs.executeScript({
-        file: "content.js"
-    }, results => {
-        CURRENT_LINKS = results[0];
-        populatePanel(results[0]);
-    });
-}
-
+/**
+ * Filter all the link details and only show those beign searched for
+ * REMARK: Currently searches for match in any of title, text, href
+ */
 function search(){
-    const searchTerm = document.querySelector("input").value;
+    const searchTerm = $("input").value;
     const validLinks = [];
     CURRENT_LINKS.forEach(link => {
         if(link.href.indexOf(searchTerm) > -1 ||
@@ -53,8 +64,71 @@ function search(){
     populatePanel(validLinks);
 }
 
-document.querySelector("#refresh").addEventListener("click", runOnActivePage);
-document.querySelector("#refresh").innerText = chrome.i18n.getMessage("refresh");
-document.querySelector("input").placeholder = chrome.i18n.getMessage("search");
-document.querySelector("input").addEventListener("input", search);
-document.head.title = chrome.i18n.getMessage("name");
+/**
+ * State of panel as per lock button
+ * @returns boolean
+ */
+function isLocked(){
+    return $('#lock.hidden');
+}
+
+/**
+ * Toggle the lock state
+ */
+function toggleLock(){
+    $("#lock").classList.toggle("hidden");
+    $("#unlock").classList.toggle("hidden");
+}
+
+/**
+ * Received an array of link details to put in panel
+ * @param links array details
+ */
+function onMsgUpdatePanel(links){
+    CURRENT_LINKS = links;
+    populatePanel(links);
+}
+
+/**
+ * Receieved a prompt that current page never ran content script
+ */
+function onMsgPromptReload(){
+    emptyLinks();
+    const msg = $$("p");
+    msg.innerText = chrome.i18n.getMessage("pagereload");
+    $("section").appendChild(msg);
+}
+
+/**
+ * Receieve messages, and check if locked then don't do anything
+ */
+chrome.runtime.onMessage.addListener((message, sender, responder) => {
+    if(isLocked()){
+        return;
+    } else if(message.updatePanel){
+        onMsgUpdatePanel(message.links);
+    } else if(message.currentPageNeedsReload){
+        onMsgPromptReload();
+    }
+});
+
+/**
+ * Init page i18n and listeners
+ */
+$("#lock").addEventListener("click", toggleLock);
+$("#lock").innerText = chrome.i18n.getMessage("lock");
+$("#unlock").addEventListener("click", toggleLock);
+$("#unlock").innerText = chrome.i18n.getMessage("unlock");
+
+$("input").addEventListener("input", search);
+$("input").placeholder = chrome.i18n.getMessage("search");
+
+document.title = chrome.i18n.getMessage("name");
+
+/**
+ * Once page loaded, request the links for current page
+ *   i.e. if opening in browser action popup
+ */
+chrome.runtime.sendMessage({
+    panelRequestLatest: true
+});
