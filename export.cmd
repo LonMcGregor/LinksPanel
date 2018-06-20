f42f9d261107dc80e7ba8b8b9c91850125cbde6c
Léon McGregor <3817332+LonMcGregor@users.noreply.github.com>
2018-06-20 11:04:27 +0100

    Better Icon

---
+++ export.cmd
@@ -0,0 +1,6 @@
+rem http://tavmjong.free.fr/INKSCAPE/MANUAL/html/CommandLine-Export.html
+"%PROGRAMFILES%\Inkscape\inkscape.exe" -e=src\16.png -C -w=16 -h=16 icons.svg
+"%PROGRAMFILES%\Inkscape\inkscape.exe" -e=src\24.png -C -w=24 -h=24 icons.svg
+"%PROGRAMFILES%\Inkscape\inkscape.exe" -e=src\32.png -C -w=32 -h=32 icons.svg
+"%PROGRAMFILES%\Inkscape\inkscape.exe" -e=src\48.png -C -w=48 -h=48 icons.svg
+"%PROGRAMFILES%\Inkscape\inkscape.exe" -e=src\128.png -C -w=128 -h=128 icons.svg
